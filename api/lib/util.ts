aa45fff07d80014b0e6495a21818f1eba8906539
memezilla <minitip@gmail.com>
2019-10-31 17:43:28 +0800

    upgrade to new version

--- api/lib/util.ts
+++ api/lib/util.ts
@@ -1,7 +1,7 @@
-import { readdirSync, lstatSync } from 'fs';
-import { join as pathJoin } from 'path';
-import * as chalk from 'chalk';
-const boxen = require('boxen'); // tilt.jpg
+import { readdirSync, lstatSync } from "fs";
+import { join as pathJoin } from "path";
+import chalk from "chalk";
+import boxen from "boxen"; // tilt.jpg
 
 export function getDirectories(srcpath: string) {
   return readdirSync(srcpath).filter(file =>
@@ -9,21 +9,20 @@ export function getDirectories(srcpath: string) {
   );
 }
 
-export function displayServer(server: any) {
-  if (process.env.NODE_ENV !== 'development') return;
+export function displayServer(port: number) {
+  if (process.env.NODE_ENV !== "development") return;
 
-  const details = server.address();
-  const localURL = `http://localhost:${details.port}`;
+  const localURL = `http://localhost:${port}`;
 
-  let message = chalk.green('Express is running!');
-  message += '\n\n';
+  let message = chalk.green("🚀 Server ready!");
+  message += "\n\n";
   message += `• ${localURL}`;
 
   console.log(
     boxen(message, {
       padding: 1,
       margin: 1,
-      borderColor: 'green',
+      borderColor: "green"
     })
   );
 }
